acb14a8416cf3fe92978d26e3c5e68fa2fa48657
ShivaKanchi <shivakanchi111@gmail.com>
2022-12-22 21:47:25 +0530

    setup

--- src/server.js
+++ src/server.js
@@ -1,13 +1,27 @@
-import express from 'express'
+const express = require('express');
 
+//files
 import dbconnection from './Database/dbconnection'
+
+
 
 const meddonor = express()
 meddonor.use(express.json())
-
+//all route
 meddonor.get("/", (req, res) => {
     res.json({
         message: "MedDonor server up",
     })
 })
-
+PORT = 4000
+meddonor.listen(PORT, () => {
+    console.log("Server is up")
+    dbconnection()
+        .then(() => {
+            console.log("Database connected")
+        })
+        .catch((error) => {
+            console.log("Db didnt conneted")
+            console.log(error)
+        })
+})
