b32441ff83ee69b936b63d191fb99843620b58af
ShivaKanchi <shivakanchi111@gmail.com>
2022-12-24 22:41:36 +0530

    Events api Working

--- src/Database/Event/index.js
+++ src/Database/Event/index.js
@@ -4,9 +4,13 @@ const eventSchema = new mongoose.Schema({
     eventname: { type: String, required: true },
     landmark: { type: String },
     address: { type: String },
-    coordinator: { type: mongoose.Types.ObjectId, required: true, ref: "users" },
+    city: { type: String },
+    state: { type: String },
     startdate: { type: String, required: true },
     enddate: { type: String, required: true },
+    //coordinator: { type: mongoose.Types.ObjectId, required: true, ref: "users" },
+    coordinator: { type: String },
+    coordinatorphno: { type: Number },
     certificate: { type: Boolean }
 }, {
     timestamps: true
