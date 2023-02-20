d4d394c47f72c056fda3f65f083a1beab3d4e072
ShivaKanchi <shivakanchi111@gmail.com>
2023-02-20 23:22:29 +0530

    Event add fix

--- src/Database/Event/index.js
+++ src/Database/Event/index.js
@@ -2,13 +2,13 @@ import mongoose from 'mongoose'
 
 const eventSchema = new mongoose.Schema({
     eventname: { type: String, required: true },
-    eventimage: { type: String },
+    eventimage: { type: String, required: true },
     landmark: { type: String },
     address: { type: String },
     city: { type: String },
     state: { type: String },
-    startdate: { type: String, required: true },
-    enddate: { type: String, required: true },
+    startdate: { type: String },
+    enddate: { type: String },
     //coordinator: { type: mongoose.Types.ObjectId, required: true, ref: "users" },
     coordinatorname: { type: String },
     coordinatorimage: { type: String },
